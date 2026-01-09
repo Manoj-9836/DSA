dd38d34d579724cd99a7f7650ed7682c9cd2b372
Manoj kumar <108039744+Manoj-9836@users.noreply.github.com>
2026-01-09 20:23:26 +0530

    Time: 4 ms (99.22%) | Memory: 98.2 MB (78.13%) - LeetSync

---
+++ 2228-watering-plants-ii/watering-plants-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minimumRefill(int[] plants, int capacityA, int capacityB) {
+        int left = 0;
+        int right = plants.length - 1;
+        
+        int waterA = capacityA;
+        int waterB = capacityB;
+        int refills = 0;
+
+        while (left < right) {
+            if (waterA < plants[left]) {
+                refills++;       
+                waterA = capacityA;
+            }
+            waterA -= plants[left]; 
+            left++;
+
+            if (waterB < plants[right]) {
+                refills++;  
+                waterB = capacityB;
+            }
+            waterB -= plants[right];
+            right--;
+        }
+
+        if (left == right) {
+            if (waterA >= waterB) {
+                if (waterA < plants[left]) {
+                    refills++;
+                }
+            } else {
+                if (waterB < plants[right]) {
+                    refills++;
+                }
+            }
+        }
+        return refills;
+    }
+}
